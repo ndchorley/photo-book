e9dc7c16a9852dd2ce31a81f283173890981cebf
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-08-12 08:30:31 +0100

    Add Shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "2.2.0"
     id("application")
     id("com.adarshr.test-logger").version("4.0.0")
+    id("com.gradleup.shadow").version("9.0.1")
 }
 
 group = "com.xyphias"
