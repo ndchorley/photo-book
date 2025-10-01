82045f6bfc74ee534005cf98fc01d464dca601bd
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-10-01 07:10:04 +0100

    Upgrade http4k to 6.18.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     implementation("org.xerial:sqlite-jdbc:3.50.3.0")
     implementation("org.flywaydb:flyway-core:11.13.2")
 
-    implementation(platform("org.http4k:http4k-bom:6.17.0.0"))
+    implementation(platform("org.http4k:http4k-bom:6.18.1.0"))
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-template-handlebars")
 
